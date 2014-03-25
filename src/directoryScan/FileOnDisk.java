b7497468306de691181dc9b50d8e354fcbf2ff5e
Devin Visslailli <devin.viss@nyu.edu>
2014-03-24 22:32:58 -0400

    Create FileOnDisk getters and setters; create constructor

--- src/directoryScan/FileOnDisk.java
+++ src/directoryScan/FileOnDisk.java
@@ -1,5 +1,41 @@
 package directoryScan;
 
 public class FileOnDisk {
+	
+	/**
+	 * @return the file
+	 */
+	public String getFile() {
+		return file;
+	}
+
+	/**
+	 * @param file the file to set
+	 */
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	/**
+	 * @return the size
+	 */
+	public float getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(float size) {
+		this.size = size;
+	}
+
+	private String file;
+	private float size;
+	
+	FileOnDisk (String file, float size) {
+		this.file = file;
+		this.size = size;
+	}
 
 }
